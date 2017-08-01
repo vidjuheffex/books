d3089ff8be0ba7f29354548f01c62ae9f2d4dd02
Julian Herrera <julian.herrera@live.com>
2017-08-01 13:42:18 -0500

    intial commit

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+
+//select book is an action creator, it will need an action type
+export function selectBook(book) {
+
+  return {
+    //You should create a Type and Payload for your Action here
+    type: "BOOK_SELECTED",
+    payload: book
+  };
+}
